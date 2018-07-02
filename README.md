07f264c018b8f6b0e3d7d115af0e8f7611bb6bf7
Gavin Matthews <gmatthews@axway.com>
2018-07-02 13:23:33 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# API Builder Flow Plugins
+
+These are V4 compatible API Builder plugins.
+
+## api-builder-plugin-gm-objectfilter
+
+A node for filtering the fields in an object.
+
+## nodehandler-gm-mustache
+
+A node similar to the dot/Compose node except this node  uses the Mustache template engine.
+
+## nodehandler-gm-ejs
+
+A node similar to the dot/Compose node except this node  uses the EJS template engine.
---
+++ api-builder-plugin-gm-objectfilter/src/index.js
@@ -0,0 +1,84 @@
+const sdk = require('axway-flow-sdk');
+const { include, exclude } = require('./action');
+
+function getFlowNodes() {
+	const flownodes = sdk.init(module);
+
+	// The unique name of your flow-node.  You can define multiple flow-nodes in this
+	// file, but one is typical.
+	flownodes.add('gm-objectfilter', {
+		name: 'Filter',
+		icon: 'icon.svg',
+		description: 'Filter the object fields.'
+	});
+
+	// Include Method
+	flownodes
+		.method('include', {
+			name: 'Include',
+			description: 'Include the selected fields.'
+		})
+		.parameter('source', {
+			description: 'The source object to filter.',
+			type: 'object'
+		})
+		.parameter('fields', {
+			description: 'The the fields to include.',
+			type: 'array',
+			items: {
+				type: 'string'
+			}
+		})
+		.output('next', {
+			name: 'Next',
+			context: '$.filtered',
+			schema: {
+				type: 'object'
+			}
+		})
+		.output('error', {
+			name: 'Error',
+			context: '$.error',
+			schema: {
+				type: 'string'
+			}
+		})
+		.action(include);
+
+	// Exclude Method
+	flownodes
+		.method('exclude', {
+			name: 'Exclude',
+			description: 'Exclude the selected fields.'
+		})
+		.parameter('source', {
+			description: 'The source object to filter.',
+			type: 'object'
+		})
+		.parameter('fields', {
+			description: 'The the fields to exclude.',
+			type: 'array',
+			items: {
+				type: 'string'
+			}
+		})
+		.output('next', {
+			name: 'Next',
+			context: '$.filtered',
+			schema: {
+				type: 'object'
+			}
+		})
+		.output('error', {
+			name: 'Error',
+			context: '$.error',
+			schema: {
+				type: 'string'
+			}
+		})
+		.action(exclude);
+
+	return Promise.resolve(flownodes);
+}
+
+exports = module.exports = getFlowNodes;
